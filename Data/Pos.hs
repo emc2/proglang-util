90c464dc9611f769e88152d53c715e7f14714edf
Eric McCorkle <eric@shadowsun.net>
2012-10-30 16:59:05 -0400

    Cosmetic changes

--- Data/Pos.hs
+++ Data/Pos.hs
@@ -1,14 +1,16 @@
-{-# OPTIONS_GHC -funbox-strict-fields #-}
 -- Copyright (c) 2012 Eric McCorkle.  All rights reserved.
 --
 -- Redistribution and use in source and binary forms, with or without
 -- modification, are permitted provided that the following conditions
 -- are met:
+--
 -- 1. Redistributions of source code must retain the above copyright
 --    notice, this list of conditions and the following disclaimer.
+--
 -- 2. Redistributions in binary form must reproduce the above copyright
 --    notice, this list of conditions and the following disclaimer in the
 --    documentation and/or other materials provided with the distribution.
+--
 -- 3. Neither the name of the author nor the names of any contributors
 --    may be used to endorse or promote products derived from this software
 --    without specific prior written permission.
@@ -25,6 +27,7 @@
 -- OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
 -- OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 -- SUCH DAMAGE.
+{-# OPTIONS_GHC -funbox-strict-fields #-}
 
 -- | This module contains utilities for representing positions in
 -- source files.  This is very commonly used in anything dealing with
